5f27ea85b047465891d066e69fb2baadbdf8b158
Oran C <oranbusiness@gmail.com>
2019-10-19 20:15:19 -0700

    inital test

---
+++ server/app.js
@@ -0,0 +1,16 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+// DANGER! This is insecure. See http://twil.io/secure
+require('dotenv').config();
+
+const accountSid = 'AC7dc40a462f2b77c470bf05603bca1963';
+const authToken = process.env.MYAPIKEY;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+15017122661',
+     to: '+15558675310'
+   })
+  .then(message => console.log(message.sid));
