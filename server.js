bcaf071098c3770921302e281b488b1cc272175a
Oran C <oranbusiness@gmail.com>
2019-10-20 10:01:41 -0700

    working

--- server.js
+++ server.js
@@ -29,38 +29,69 @@ app.post('/sms', (req, res) => {
 
 
     let page_text = get_page_text(phone)
-    let current = current_page(phone);
-    if(!db.get("users").find({phone:phone}).has("page_name").value()){
-        db.get("users").find({phone:phone}).set("page_name","welcome").write()
-    }
 
-    let isUser = db.get("users").some(user => user.phone === phone).value()
+    let current = db.get("users").find({ phone: phone }).get("page_name").value()
+   
+    let user_exists = db.get("users").some(user => user.phone === phone).value()
 
-    if(!isUser){
-        console.log("successfull")
-        db.get("users").push({phone:phone,page_name:"welcome"}).write()
-        twiml.message("Adding phone number to database");
-    }
-        
-    let num_links = db.get(`pages.${current}.links`).value().length
-    
+    let ispage = db.has(`pages.${current}.links[${text_input}]`).value()
+    let isSelection = db.get(`pages.${current}.type`).value()
 
-    if(!isNaN(text_input) && parseInt(text_input) < num_links && parseInt(text_input) >= 0 ){
-    
-        let page = db.get(`pages.${current}.links[${text_input}]`).value()
-        if(page){
-        db.get("users").find({ phone: req.body.From }).set("page_name",page).write()
-        }
-        console.log(db.get("users").find({ phone: req.body.From }).has("page_name").value())
-        console.log(db.get("users").find({ phone: req.body.From }).value())
+    if(ispage && isSelection == "select"){
+        console.log("page exists and page type == select")
+        console.log(`pages.${current}.links[${text_input}]`)
+        let next_page = db.get(`pages.${current}.links[${text_input}]`).value()
+        console.log(next_page)
+        db.get("users").find({ phone: phone }).assign({ page_name: next_page, phone:phone}).write()
+        twiml.message(get_page_text(phone))
+
+    }else if(user_exists && isSelection == "input" && current == "description"){
+        console.log("user exists and page  == description")
+        let next_page = db.get(`pages.${current}.links[${text_input}]`).value()
+
+        db.get("users").find({ phone: phone }).assign({intro:text_input}).write()
+        console.log(db.get("users").find({ phone: phone }).value())
+
+    }else if(user_exists && isSelection == "input" &&  current == "location"){
+        console.log("user exists and page type == input and page ==location")
+        console.log(text_input)
+        let next_page = db.get(`pages.${current}.links[${text_input}]`).value()
+
+        db.get("users").find({ phone: phone }).assign({location:text_input}).write()
+        console.log(db.get("users").find({ phone: phone }).value())
+
+        db.get("users").find({ phone: phone }).assign({ page_name: next_page, phone:phone}).write()
+        twiml.message(get_page_text(phone))
+
     }else{
-        // twiml.message("Something went wrong...")
-        console.log("fail")
+        console.log("page_name is not set")
+        twiml.message("page_name is not set")
+    }
+
+    if(user_exists && isNaN(text_input)){
+        console.log("user input is text")
+        twiml.message(get_page_text(phone))
+    }else{
+        console.log("user does not exist")
+    }
+
+    if(!user_exists){
+        console.log("create a new user")
+        db.get('users')
+        .push({
+            "phone": phone,
+            "page_name": "welcome",
+            "location":"",
+            "intro":"",
+            "help":""
+          })
+        .write()
+    
+    
+        twiml.message(db.get("pages.welcome.text").value() )
+
     }
     
-    
-    twiml.message(page_text)
-
     res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(twiml.toString());
 });
