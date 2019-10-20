c1de989c7726276cda8906c0c5ba5926434c022f
wisehackermonkey <oranbusiness@gmail.com>
2019-10-20 11:23:19 -0700

    broken but working,

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('db.json')
 const db = low(adapter)
 
-
+let s = 0
 
 const PORT = 80
 const app = express();
@@ -30,60 +30,76 @@ app.post('/sms', (req, res) => {
 
     let page_text = get_page_text(phone)
 
-    let current = db.get("users").find({ phone: phone }).get("page_name").value()
    
     let user_exists = db.get("users").some(user => user.phone === phone).value()
-
-    let ispage = db.has(`pages.${current}.links[${text_input}]`).value()
-    let isSelection = db.get(`pages.${current}.type`).value()
-
-    if(ispage && isSelection == "select"){
-        console.log("page exists and page type == select")
-        console.log(`pages.${current}.links[${text_input}]`)
-        let next_page = db.get(`pages.${current}.links[${text_input}]`).value()
-        console.log(next_page)
-        db.get("users").find({ phone: phone }).assign({ page_name: next_page, phone:phone}).write()
-        twiml.message(get_page_text(phone))
-
-    }else if(user_exists && isSelection == "input" && current == "description"){
-        console.log("user exists and page  == description")
-        let next_page = db.get(`pages.${current}.links[${text_input}]`).value()
-
-        db.get("users").find({ phone: phone }).assign({intro:text_input}).write()
+    if(user_exists){
+        let current = db.get("users").find({ phone: phone }).get("page_name").value()
+        console.log("user exists set current = " +current)
         console.log(db.get("users").find({ phone: phone }).value())
+    
+        let ispage = db.has(`pages.${current}.links[${text_input}]`).value()
+        let isSelection = db.get(`pages.${current}.type`).value()
 
-    }else if(user_exists && isSelection == "input" &&  current == "location"){
-        console.log("user exists and page type == input and page ==location")
-        console.log(text_input)
-        let next_page = db.get(`pages.${current}.links[${text_input}]`).value()
+        console.log(current)
+        if(ispage && isSelection == "select"){
+            console.log("page exists and page type == select")
+            console.log(`pages.${current}.links[${text_input}]`)
+            let next_page = db.get(`pages.${current}.links[${text_input}]`).value()
+            console.log(next_page)
+            db.get("users").find({ phone: phone }).assign({ page_name: next_page, phone:phone}).write()
+            twiml.message(get_page_text(phone))
 
-        db.get("users").find({ phone: phone }).assign({location:text_input}).write()
-        console.log(db.get("users").find({ phone: phone }).value())
+            if(current == "sent"){
+                twiml.message("Your request has been accepted by a Suraksha fellow! Please contact the following number to get in touch with her: 707-494-6135")
 
-        db.get("users").find({ phone: phone }).assign({ page_name: next_page, phone:phone}).write()
-        twiml.message(get_page_text(phone))
+            }
+        }
+        else if(user_exists && isSelection == "input" && current == "description"){
+            console.log("user exists and page  == description")
+
+            db.get("users").find({ phone: phone }).assign({intro:text_input}).write()
+            console.log(db.get("users").find({ phone: phone }).value())
+            twiml.message(get_page_text(phone))
+            db.get("users").find({ phone: phone }).assign({ page_name: "sent", phone:phone}).write()
+
+        }else if(user_exists && isSelection == "input" &&  current == "location"){
+            console.log("user exists and page type == input and page ==location")
+            console.log(text_input)
+
+            db.get("users").find({ phone: phone }).assign({location:text_input}).write()
+            console.log(db.get("users").find({ phone: phone }).value())
+
+            twiml.message(get_page_text(phone))
+
+            db.get("users").find({ phone: phone }).assign({ page_name: "description", phone:phone}).write()
+            
+
+        }else{
+            console.log("page_name is not set")
+            twiml.message("Thanks! Your request has been sent. We'll send you an update once someone responds to your request.")
+            twiml.message("Your request has been accepted by a Suraksha fellow! Please contact the following number to get in touch with her: 707-494-6135")
+
+        }
 
     }else{
-        console.log("page_name is not set")
-        twiml.message("page_name is not set")
+        let current = ""
     }
-
     if(user_exists && isNaN(text_input)){
         console.log("user input is text")
-        twiml.message(get_page_text(phone))
+        // twiml.message(get_page_text(phone))
     }else{
         console.log("user does not exist")
     }
 
     if(!user_exists){
+        twiml.message("creating a new user")
         console.log("create a new user")
         db.get('users')
         .push({
             "phone": phone,
             "page_name": "welcome",
             "location":"",
-            "intro":"",
-            "help":""
+            "intro":""
           })
         .write()
     
